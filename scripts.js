1dd020539ff53c42c6a6d8d16ad497895c5546bc
Akido123 <117109568+Akido123@users.noreply.github.com>
2023-05-30 22:06:20 +0200

    Update: added a comment.

--- scripts.js
+++ scripts.js
@@ -10,11 +10,11 @@ form.addEventListener("submit", (event) => {
   if(dividend === '' || divider === '') { // Added a condition to detect if either inputs are empty and display a set of text.
     result.innerText = 'Division not performed. Both values are required in inputs. Try again.'
   }
-  else if(dividend <= 0 || divider <= 0){ // Added a condition to detect if either inputs are negative, throw an error and display a set of text 
+  else if(dividend <= 0 || divider <= 0){ // Added a condition to detect if either inputs are negative, throw an error and display a set of text.
     result.innerText = 'Division not performed. Invalid number provided. Try again.'
     throw new Error(console.trace("Call stack: "))
   }
-  else if(typeof(dividend) !== typeof(NaN) || typeof(divider) !== typeof(NaN)){
+  else if(typeof(dividend) !== typeof(NaN) || typeof(divider) !== typeof(NaN)){ // Added a condition to detect if either  inputs are not a number, throw an error and display a set of text.
     document.body.innerHTML = "Something critical went wrong. Please reload the page."
     throw new Error(console.trace("Call stack: "))
   }
